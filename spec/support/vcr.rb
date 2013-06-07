72f01f2b7545deab94bd2665bf721849dc8d85d5
Andrew Assarattanakul <assarata@gmail.com>
2013-06-07 15:50:14 -0500

    Updated cassettes for vcr v2.0

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,9 +1,9 @@
 # encoding: UTF-8
 
-VCR.config do |config|
+VCR.configure do |config|
   config.cassette_library_dir = File.expand_path('../../fixtures/net', __FILE__)
   config.default_cassette_options = { :record => :none }
-  config.stub_with :webmock
+  config.hook_into :webmock
   config.filter_sensitive_data('%{API_USERNAME}') { AVALARA_CONFIGURATION['username'] }
   config.filter_sensitive_data('%{API_PASSWORD}') { AVALARA_CONFIGURATION['password'] }
   config.filter_sensitive_data('%{API_ENDPOINT}') { AVALARA_CONFIGURATION['endpoint'] }
