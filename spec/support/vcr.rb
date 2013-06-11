ad49e589d4febea98747b9e8f22be29634b0683b
Andrew Assarattanakul <assarata@gmail.com>
2013-06-11 09:44:39 -0500

    Fixed api endpoint to match the host instead of the full url. Combined api username and api password to api basic auth and encoded it so special characters will match.

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,12 +1,19 @@
 # encoding: UTF-8
 
+require 'uri'
 VCR.configure do |config|
+  def uri_encode(value)
+    URI.encode(value, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+  end
+
   config.cassette_library_dir = File.expand_path('../../fixtures/net', __FILE__)
   config.default_cassette_options = { :record => :none }
   config.hook_into :webmock
-  config.filter_sensitive_data('%{API_USERNAME}') { AVALARA_CONFIGURATION['username'] }
-  config.filter_sensitive_data('%{API_PASSWORD}') { AVALARA_CONFIGURATION['password'] }
-  config.filter_sensitive_data('%{API_ENDPOINT}') { AVALARA_CONFIGURATION['endpoint'] }
+
+  config.filter_sensitive_data('API_BASIC_AUTH') {
+    "#{uri_encode(AVALARA_CONFIGURATION['username'])}:#{uri_encode(AVALARA_CONFIGURATION['password'])}"
+  }
+  config.filter_sensitive_data('API_ENDPOINT') { URI.parse(AVALARA_CONFIGURATION['endpoint']).host }
 end
 
 RSpec.configure do |config|
