e5aff368af47f4eea696e6ba5a8096160eae5e3f
Andrew Assarattanakul <assarata@gmail.com>
2013-06-11 12:15:04 -0500

    Added error message fields for tax response.

--- lib/avalara/response/tax.rb
+++ lib/avalara/response/tax.rb
@@ -1,9 +1,23 @@
 module Avalara
   module Response
     class Tax < Avalara::Types::Stash
-      property :rate,        :from => 'Rate'
-      property :tax,         :from => 'Tax'
-      property :tax_details, :from => 'TaxDetails'
+      property :rate,        :from => :Rate
+      property :tax,         :from => :Tax
+      property :tax_details, :from => :TaxDetails
+
+      property :result_code, :from => :ResultCode
+      property :messages,    :from => :Messages
+
+      def success?
+        result_code != 'Error'
+      end
+
+      def Messages=(new_messages)
+        self.messages = []
+        new_messages.each do |message|
+          self.messages << Message.new(message)
+        end
+      end
 
       def TaxDetails=(details)
         self.tax_details = details.map { |d| Avalara::Response::TaxDetail.new(d) }
