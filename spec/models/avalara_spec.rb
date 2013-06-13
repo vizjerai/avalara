6df4600bb2a580cbe973f65dc00201b20740135c
Andrew Assarattanakul <assarata@gmail.com>
2013-06-13 09:26:47 -0500

    Fixed deprecation warnings and restructured the cassettes to follow the newer pattern.

--- spec/models/avalara_spec.rb
+++ spec/models/avalara_spec.rb
@@ -98,9 +98,49 @@ describe Avalara do
     let(:invoice) { FactoryGirl.build_via_new(:invoice, doc_date: doc_date) }
     let(:request) { Avalara.get_tax(invoice) }
 
-    context 'failure', :vcr do
+    it 'successful response', :vcr do
+      expect(request).to be_kind_of Avalara::Response::Invoice
+      expect(request).to be_success
+
+      expect(request.doc_code).to_not be_nil
+      expect(request.doc_date).to eq "2012-01-01"
+      expect(request.result_code).to eq "Success"
+      expect(request.tax_date).to_not be_nil
+      expect(request.timestamp).to_not be_nil
+      expect(request.total_amount).to eq "10"
+      expect(request.total_discount).to eq "0"
+      expect(request.total_exemption).to eq "10"
+      expect(request.total_tax).to eq "0"
+      expect(request.total_tax_calculated).to eq "0"
+
+      expect(request.tax_lines.length).to be 1
+      tax_line = request.tax_lines.first
+      expect(tax_line.line_no).to eq "1"
+      expect(tax_line.tax_code).to eq "P0000000"
+      expect(tax_line.taxability).to eq "true"
+      expect(tax_line.taxable).to eq "0"
+      expect(tax_line.rate).to eq "0"
+      expect(tax_line.tax).to eq "0"
+      expect(tax_line.discount).to eq "0"
+      expect(tax_line.tax_calculated).to eq "0"
+      expect(tax_line.exemption).to eq "10"
+      expect(tax_line.tax_details.length).to be 1
+
+      tax_detail = tax_line.tax_details.first
+      expect(tax_detail.taxable).to eq "0"
+      expect(tax_detail.rate).to eq "0"
+      expect(tax_detail.tax).to eq "0"
+      expect(tax_detail.region).to eq "WA"
+      expect(tax_detail.country).to eq "US"
+      expect(tax_detail.juris_type).to eq "State"
+      expect(tax_detail.juris_name).to eq "WASHINGTON"
+      expect(tax_detail.tax_name).to eq "WA STATE TAX"
+
+      expect(request.tax_addresses.length).to be 1
+    end
+
+    context 'with missing required field', :vcr do
       let(:invoice) { FactoryGirl.build_via_new(:invoice, customer_code: nil) }
-      use_vcr_cassette 'get_tax/failure'
 
       it 'unsuccessful response' do
         expect(request).to be_kind_of Avalara::Response::Invoice
@@ -121,84 +161,36 @@ describe Avalara do
         expect { request }.to raise_error(Avalara::TimeoutError)
       end
     end
-
-    context 'success', :vcr do
-      use_vcr_cassette 'get_tax/success'
-
-      it 'successful response' do
-        expect(request).to be_kind_of Avalara::Response::Invoice
-        expect(request).to be_success
-
-        expect(request.doc_code).to_not be_nil
-        expect(request.doc_date).to eq "2012-01-01"
-        expect(request.result_code).to eq "Success"
-        expect(request.tax_date).to_not be_nil
-        expect(request.timestamp).to_not be_nil
-        expect(request.total_amount).to eq "10"
-        expect(request.total_discount).to eq "0"
-        expect(request.total_exemption).to eq "10"
-        expect(request.total_tax).to eq "0"
-        expect(request.total_tax_calculated).to eq "0"
-
-        expect(request.tax_lines.length).to be 1
-        tax_line = request.tax_lines.first
-        expect(tax_line.line_no).to eq "1"
-        expect(tax_line.tax_code).to eq "P0000000"
-        expect(tax_line.taxability).to eq "true"
-        expect(tax_line.taxable).to eq "0"
-        expect(tax_line.rate).to eq "0"
-        expect(tax_line.tax).to eq "0"
-        expect(tax_line.discount).to eq "0"
-        expect(tax_line.tax_calculated).to eq "0"
-        expect(tax_line.exemption).to eq "10"
-        expect(tax_line.tax_details.length).to be 1
-
-        tax_detail = tax_line.tax_details.first
-        expect(tax_detail.taxable).to eq "0"
-        expect(tax_detail.rate).to eq "0"
-        expect(tax_detail.tax).to eq "0"
-        expect(tax_detail.region).to eq "WA"
-        expect(tax_detail.country).to eq "US"
-        expect(tax_detail.juris_type).to eq "State"
-        expect(tax_detail.juris_name).to eq "WASHINGTON"
-        expect(tax_detail.tax_name).to eq "WA STATE TAX"
-
-        expect(request.tax_addresses.length).to be 1
-      end
-    end
   end
 
   describe '.geographical_tax' do
     let(:latitude) { '47.627935' }
+    let(:longitude) { '-122.51702' }
     let(:sales_amount) { 100 }
     let(:request) { Avalara.geographical_tax(latitude, longitude, sales_amount) }
 
-    context 'success', :vcr do
-      let(:longitude) { '-122.51702' }
-      use_vcr_cassette 'geographical_tax/success'
-      it 'successful response' do
-        expect(request).to be_kind_of Avalara::Response::Tax
-        expect(request).to be_success
+    it 'successful response', :vcr do
+      expect(request).to be_kind_of Avalara::Response::Tax
+      expect(request).to be_success
 
-        expect(request.rate).to eq 0.086
-        expect(request.tax).to eq 8.6
-        expect(request.tax_details).to have(2).items
+      expect(request.rate).to eq 0.086
+      expect(request.tax).to eq 8.6
+      expect(request.tax_details).to have(2).items
 
-        state_tax_detail = request.tax_details.first
-        expect(state_tax_detail.rate).to eq 0.065
-        expect(state_tax_detail.tax).to eq 6.5
-        expect(state_tax_detail.juris_type).to eq 'State'
+      state_tax_detail = request.tax_details.first
+      expect(state_tax_detail.rate).to eq 0.065
+      expect(state_tax_detail.tax).to eq 6.5
+      expect(state_tax_detail.juris_type).to eq 'State'
 
-        city_tax_detail = request.tax_details.last
-        expect(city_tax_detail.rate).to eq 0.021
-        expect(city_tax_detail.tax).to eq 2.1
-        expect(city_tax_detail.juris_type).to eq 'City'
-      end
+      city_tax_detail = request.tax_details.last
+      expect(city_tax_detail.rate).to eq 0.021
+      expect(city_tax_detail.tax).to eq 2.1
+      expect(city_tax_detail.juris_type).to eq 'City'
     end
 
-    context 'failure', :vcr do
+    context 'with incomplete address', :vcr do
       let(:longitude) { '122.51702' }
-      use_vcr_cassette 'geographical_tax/failure'
+
       it 'unsuccessful response' do
         expect(request).to be_kind_of Avalara::Response::Tax
         expect(request).to_not be_success
@@ -222,39 +214,36 @@ describe Avalara do
     let(:address) { FactoryGirl.build_via_new(:address) }
     let(:request) { Avalara.validate_address(address) }
 
-    context 'failure', :vcr do
+    it 'successful response', :vcr do
+      expect(request).to be_kind_of Avalara::Response::AddressValidation
+      expect(request).to be_success
+
+      expect(request.address).to be_a Avalara::Response::ValidationAddress
+
+      address = request.address
+      expect(address.line_1).to eq '250'
+      expect(address.line_2).to eq '435 Ericksen Ave NE'
+      expect(address.line_3).to be_nil
+      expect(address.city).to eq 'Bainbridge Island'
+      expect(address.region).to eq 'WA'
+      expect(address.country).to eq 'US'
+      expect(address.address_type).to eq 'H'
+      expect(address.address_type_name).to eq 'High-rise or business complex'
+      expect(address.postal_code).to eq '98110-1896'
+      expect(address.county).to eq 'Kitsap'
+      expect(address.fips_code).to eq '5303500000'
+      expect(address.carrier_route).to eq 'C051'
+      expect(address.carrier_route_name).to eq 'City delivery'
+      expect(address.tax_region_id).to be_nil
+      expect(address.post_net).to eq '981101896999'
+    end
+
+    context 'with missing postal code', :vcr do
       let(:address) { FactoryGirl.build_via_new(:address, :postal_code => nil) }
-      use_vcr_cassette 'validate_address/failure'
+
       it 'unsuccessful response' do
         expect(request).to be_kind_of Avalara::Response::AddressValidation
         expect(request).to_not be_success
-      end
-    end
-
-    context 'success', :vcr do
-      use_vcr_cassette 'validate_address/success'
-      it 'successful response' do
-        expect(request).to be_kind_of Avalara::Response::AddressValidation
-        expect(request).to be_success
-
-        expect(request.address).to be_a Avalara::Response::ValidationAddress
-
-        address = request.address
-        expect(address.line_1).to eq '250'
-        expect(address.line_2).to eq '435 Ericksen Ave NE'
-        expect(address.line_3).to be_nil
-        expect(address.city).to eq 'Bainbridge Island'
-        expect(address.region).to eq 'WA'
-        expect(address.country).to eq 'US'
-        expect(address.address_type).to eq 'H'
-        expect(address.address_type_name).to eq 'High-rise or business complex'
-        expect(address.postal_code).to eq '98110-1896'
-        expect(address.county).to eq 'Kitsap'
-        expect(address.fips_code).to eq '5303500000'
-        expect(address.carrier_route).to eq 'C051'
-        expect(address.carrier_route_name).to eq 'City delivery'
-        expect(address.tax_region_id).to be_nil
-        expect(address.post_net).to eq '981101896999'
       end
     end
 
--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -6,16 +6,13 @@ VCR.configure do |config|
     URI.encode(value, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
   end
 
-  config.cassette_library_dir = File.expand_path('../../fixtures/net', __FILE__)
+  config.cassette_library_dir = 'spec/fixtures/net'
   config.default_cassette_options = { :record => :once }
   config.hook_into :webmock
+  config.configure_rspec_metadata!
 
   config.filter_sensitive_data('API_BASIC_AUTH') {
     "#{uri_encode(AVALARA_CONFIGURATION['username'])}:#{uri_encode(AVALARA_CONFIGURATION['password'])}"
   }
   config.filter_sensitive_data('API_ENDPOINT') { URI.parse(AVALARA_CONFIGURATION['endpoint']).host }
-end
-
-RSpec.configure do |config|
-  config.extend(VCR::RSpec::Macros)
 end
